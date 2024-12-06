c2665d203ae21bbfe078c938aefca900aa9c3d94
Guilhermo González <guilhermomg@gmail.com>
2024-12-06 15:39:36 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,10 @@
+# main.tf - Arquivo principal de configuração do Terraform
+
+provider "aws" {
+  region = "us-east-1"  # Região da AWS
+}
+
 module "app_runner" {
-  source = "./modules/app_runner"
-  env    = var.env
+  source = "./modules/app_runner"  # Caminho para o módulo que provisiona o AppRunner
+  env    = var.env                 # Variável para especificar o ambiente (dev ou prod)
 }
